c64c13011ccca889dcea42e7745579bf4b53b6df
BlindBlocArt-Jerry <42042199+BlindBlocArt-Jerry@users.noreply.github.com>
2018-11-01 11:48:31 -0700

    Create initial_block_producers.c

---
+++ initial_block_producers.c
@@ -0,0 +1,47 @@
+Block Producer Name       Owner Public Key                                            Active Public Key
+blindblocart              EOS7SuVP2Us3fRxKMYmrR6NzgRa4wm5QWEE3F8V9d6LpzPjGEGFKh       EOS7p6ogWw2cfyCwRWp3sbhLZ3hewUYJYtyKUPEs9wqZUHyNP57QL
+infinitybloc              EOS8QU9ZdAeytzFozo3He1skbrZoZ6ig2EkLV7zR7ohMhfD5nJJSP       EOS6DKqyXhhQdejJHNNAuac7KE9cmoCoWoYY7i5AKwXFtALK6Rx9p
+goodblocktls
+kainosblkpro
+caleosblocks
+bigironbptex
+tlsvenezuela
+telosdacnode
+swedencornet
+octagonlabs3
+eosbarcelona
+tlsvancouver
+telosmadrid1
+telosnewyork
+telosmiamibp
+eosawakeiobp
+telosvoyager
+amplifiedtls
+eosmetaliobp
+telosfinexbp
+telasiachina
+bpeosindexio
+telosimpera1
+beyondbtctls
+eosiodetroit
+southafricas
+eossandiego1
+telosarabia1
+telosflorida
+telosinspace
+telgoingos13
+telosuknodes
+telosunlimit
+teloscantons
+telosintasia
+telosgreenbp
+cryptosuviio
+eosoup2pnode
+happyholders
+21zephyr1111
+tlosmatrix24
+droneeenergy
+telosamerica
+telosglobal1
+telossuperbp
+votedutcheos
